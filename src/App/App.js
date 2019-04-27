12c8aeae2745953c916f007a3b178f7d168577e5
AlexGaiser <alexcgaiser@gmail.com>
2019-04-27 19:26:50 -0400

    adds idgenerator logic, creates common.js file that will have all common functions used in different parts of the app.

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import { SessionMaker } from './components/SessionMaker/SessionMaker';
 
 function App() {
   return (
-    <div><div className="App">
-      Meet Me Halfway
-    </div>
+    <div>
+      <div className="App">
+        Meet Me Halfway
+      </div>
+      <SessionMaker/>
     </div>
   );
 }
