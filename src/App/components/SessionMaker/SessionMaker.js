5f988fdfee0de52a86697c0c2d6eb69447b2ef57
AlexGaiser <alexcgaiser@gmail.com>
2019-04-27 17:35:00 -0400

    change reference to App.js in index.js

--- src/App/components/SessionMaker/SessionMaker.js
+++ src/App/components/SessionMaker/SessionMaker.js
@@ -5,7 +5,7 @@ import LinkGenerator from '../LinkGenerator/LinkGenerator'
 export const SessionMaker = (props) => {
   return (
     <React.Fragment>
-      <h1>This is where we create a session</h1>
+      <h1>This is where we create a session and define it's parameters</h1>
       <LinkGenerator/>
     </React.Fragment>
   )
