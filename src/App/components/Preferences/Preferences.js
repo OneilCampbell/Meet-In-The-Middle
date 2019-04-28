330de4cb7954725a90a880137cd22e1f0895de90
Stanly Rodriguez <ibestanly@gmail.com>
2019-04-28 07:54:59 -0400

    fix fonts

--- src/App/components/Preferences/Preferences.js
+++ src/App/components/Preferences/Preferences.js
@@ -1,20 +1,54 @@
-import React, { Component } from 'react';
-import './style.css'
-import '../../App.css'
-import PrefBox from '../PrefBox/PrefBox';
+import React, { Component } from "react";
+import "./style.css";
+import "../../App.css";
+import PrefBox from "../PrefBox/PrefBox";
 
 class Preferences extends Component {
   constructor(props) {
     super(props);
-    this.state = {  }
+    this.state = {};
   }
-  hearArray = ['Live Music', 'Jazz', 'Pop', 'Hard Rock', 'Classical', 'Folk', '+ add your own tag']
+  hearArray = [
+    "Live Music",
+    "Jazz",
+    "Pop",
+    "Hard Rock",
+    "Classical",
+    "Folk",
+    "+ add your own tag"
+  ];
 
-  eatArray = ['American', 'Chinese', 'Italian', 'French', 'Greek', 'Japanese', 'Thai', 'Burgers', '+ add your own tag']
+  eatArray = [
+    "American",
+    "Chinese",
+    "Italian",
+    "French",
+    "Greek",
+    "Japanese",
+    "Thai",
+    "Burgers",
+    "+ add your own tag"
+  ];
 
-  doArray = ['Billiards', 'Dance', 'Karaoke', 'Bowling', 'Golf', '+ add your own tag']
+  doArray = [
+    "Billiards",
+    "Dance",
+    "Karaoke",
+    "Bowling",
+    "Golf",
+    "+ add your own tag"
+  ];
 
-  drinkArray =['Cocktails', 'Beer', 'Wine', 'Coffee', 'Bubble Tea', 'Whiskey', 'Ice Cream Floats', '+ add your own tag']
+  drinkArray = [
+    "Cocktails",
+    "Beer",
+    "Wine",
+    "Coffee",
+    "Bubble Tea",
+    "Whiskey",
+    "Ice Cream Floats",
+    "+ add your own tag"
+  ];
 
   // handlePress =(event)=>{
   //   const {selected} = (event.target)
@@ -24,31 +58,30 @@ class Preferences extends Component {
   //   })
   // }
 
-
-  render() { 
-    return ( 
-      <div className= 'preference-container'>
-        <h1 className='card-title'>Choose Preferences</h1>
-        <h3 className='pick-subheading' >pick all that apply</h3>
+  render() {
+    return (
+      <div className="preference-container">
+        <h1 className="card-title">Choose preferences</h1>
+        <h3 className="pick-subheading">pick all that apply</h3>
         <div>
-        <h3 className='preference-heading'>What do you want to hear?</h3>
-        <PrefBox prefList={this.hearArray} />
+          <h3 className="preference-heading">What do you want to hear?</h3>
+          <PrefBox prefList={this.hearArray} />
         </div>
         <div>
-        <h3 className='preference-heading'>What do you want to eat?</h3>
-        <PrefBox prefList={this.eatArray} />
+          <h3 className="preference-heading">What do you want to eat?</h3>
+          <PrefBox prefList={this.eatArray} />
         </div>
         <div>
-        <h3 className='preference-heading'>What do you want to do?</h3>
-        <PrefBox prefList={this.doArray} />
+          <h3 className="preference-heading">What do you want to do?</h3>
+          <PrefBox prefList={this.doArray} />
         </div>
         <div>
-        <h3 className='preference-heading'>What do you want to drink?</h3>
-        <PrefBox prefList={this.drinkArray} />
+          <h3 className="preference-heading">What do you want to drink?</h3>
+          <PrefBox prefList={this.drinkArray} />
         </div>
       </div>
-     );
+    );
   }
 }
- 
+
 export default Preferences;
--- src/App/components/WaitingRoom/Waitingroom.js
+++ src/App/components/WaitingRoom/Waitingroom.js
@@ -1,19 +1,24 @@
-import React from 'react'
+import React from "react";
 import "./style.css";
 import "../../App.css";
 
 const WaitingRoom = () => {
-
-  return ( 
+  return (
     <div className="waiting-room">
-      <p className="card-title where-to-meet">Where will you meet?</p>
+      <p className="where-to-meet">Where will you meet?</p>
       <div className="waiting-room-image" />
-      <p className="want-to-be-notified">Want to be notified when everyone has responded?</p>
-      <input className="phone-number-text-box" type="text" placeholder="Enter your phone number" />
+      <p className="want-to-be-notified">
+        Want to be notified when everyone has responded?
+      </p>
+      <input
+        className="phone-number-text-box"
+        type="text"
+        placeholder="Enter your phone number"
+      />
       <p className="send-a-text">We'll send you a text</p>
       <button className="waiting-page-submit">Submit</button>
     </div>
-   );
-}
- 
+  );
+};
+
 export default WaitingRoom;
