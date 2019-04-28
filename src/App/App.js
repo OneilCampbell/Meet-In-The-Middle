d6b74fea8cc7a25c0fd4bcf47676f0cfdfda7c69
Stanly Rodriguez <ibestanly@gmail.com>
2019-04-28 10:03:00 -0400

    uncommented carasel

--- src/App/App.js
+++ src/App/App.js
@@ -1,74 +1,73 @@
-
-import React, { Component } from 'react';
-import './App.css';
-import { SessionMaker } from './components/SessionMaker/SessionMaker';
-import Login from './components/Login/Login';
-import Profile from './components/Profile/Profile'
-import ProfilePage from './components/Profile/Profile';
-import CreateEventPage from './components/CreateEventPage/CreateEventPage';
-import Preferences from './components/Preferences/Preferences';
-import InvitePage from './components/InvitePage/InvitePage';
-import Confirmation from './components/Confirmation/Confirmation';
-import LinkPage from './components/LinkPage/LinkPage';
-import WaitingRoom from './components/WaitingRoom/WaitingRoom';
-import ResultsPage from './components/ResultsPage/ResultsPage';
-import Header from './components/Header/Header';
-import LandingPage from './components/LandingPage/LandingPage';
-
-
+import React, { Component } from "react";
+import "./App.css";
+import { SessionMaker } from "./components/SessionMaker/SessionMaker";
+import Login from "./components/Login/Login";
+import Profile from "./components/Profile/Profile";
+import ProfilePage from "./components/Profile/Profile";
+import CreateEventPage from "./components/CreateEventPage/CreateEventPage";
+import Preferences from "./components/Preferences/Preferences";
+import InvitePage from "./components/InvitePage/InvitePage";
+import Confirmation from "./components/Confirmation/Confirmation";
+import LinkPage from "./components/LinkPage/LinkPage";
+import WaitingRoom from "./components/WaitingRoom/WaitingRoom";
+import ResultsPage from "./components/ResultsPage/ResultsPage";
+import Header from "./components/Header/Header";
+import LandingPage from "./components/LandingPage/LandingPage";
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { 
-      viewIndex: 0,
-     }
+    this.state = {
+      viewIndex: 0
+    };
   }
 
   VIEWS = [
-    <LandingPage/>,
-    <CreateEventPage/>,
-    <Preferences/>,
+    <LandingPage />,
+    <CreateEventPage />,
+    <Preferences />,
     <InvitePage />,
     <Confirmation />,
     <LinkPage />,
     <WaitingRoom />,
     <ResultsPage />,
     <div>End of List</div>
-  ]
-  backView=()=>{
-    if(this.state.viewIndex > 0){
-      this.setState({viewIndex: this.state.viewIndex-1})
+  ];
+  backView = () => {
+    if (this.state.viewIndex > 0) {
+      this.setState({ viewIndex: this.state.viewIndex - 1 });
     }
-  }
+  };
 
-  nextView=() =>{
-    if(this.state.viewIndex < this.VIEWS.length-1){
-      this.setState({viewIndex: this.state.viewIndex+1})
+  nextView = () => {
+    if (this.state.viewIndex < this.VIEWS.length - 1) {
+      this.setState({ viewIndex: this.state.viewIndex + 1 });
     }
-  }
+  };
 
   render() {
     return (
-
-    <div>
-      <Header />
-      <div className="App">
-      </div>
-      {/* <Login/> */}
-      <div className='card-container'>
-        {/* {this.VIEWS[this.state.viewIndex]} */}
-        <ResultsPage />
-        <div className='button-nav'>
-          <button className={'nav-button'} onClick ={this.backView}>Back</button>
-          <button  className={'nav-button'} onClick={this.nextView}>Next</button>
+      <div>
+        <Header />
+        <div className="App" />
+        {/* <Login/> */}
+        <div className="card-container">
+          {this.VIEWS[this.state.viewIndex]}
+          {/* <ResultsPage /> */}
+          <div className="button-nav">
+            <button className={"nav-button"} onClick={this.backView}>
+              Back
+            </button>
+            <button className={"nav-button"} onClick={this.nextView}>
+              Next
+            </button>
+          </div>
         </div>
+
+        <div className="footer" />
       </div>
-     
-      <div className='footer'></div>
-    </div>
-  );
+    );
   }
 }
 
