3d035473fed5c4bf1d8cabe4ce39baad92a14c64
Stanly Rodriguez <ibestanly@gmail.com>
2019-04-28 00:14:28 -0400

    add invite page basic html

--- src/App/App.js
+++ src/App/App.js
@@ -1,16 +1,14 @@
-import React from 'react';
-import './App.css';
-import { SessionMaker } from './components/SessionMaker/SessionMaker';
-import Login from './components/Login/Login';
+import React from "react";
+import "./App.css";
+import { SessionMaker } from "./components/SessionMaker/SessionMaker";
+import Login from "./components/Login/Login";
 
 function App() {
   return (
     <div>
-      <div className="App">
-        Meet Me Halfway
-      </div>
-      <Login/>
-      <SessionMaker/>
+      <div className="App">Meet Me Halfway</div>
+      <Login />
+      <SessionMaker />
     </div>
   );
 }
